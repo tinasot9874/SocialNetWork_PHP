0a633e00aac4e3c37039525c38d9bb2e018888c6
Hieu Tran <33086916+tinasot9874@users.noreply.github.com>
2020-03-12 04:05:13 +0700

    + add code handle timeframe for posting a post

--- includes/classes/Post.php
+++ includes/classes/Post.php
@@ -65,6 +65,59 @@ class Post{
             $user_details_query = mysqli_query($this->conn, "SELECT first_name, last_name, profile_pic FROM users WHERE username = '$added_by'");
             $user_row = mysqli_fetch_array($user_details_query);
 
+            // Timeframe
+
+
+            $date_time_now = date("d-m-Y H:i:s");
+            $start_date = new DateTime($date_time);     // Time of post
+            $end_date = new DateTime($date_time_now);   // Current Time
+            $interval = $start_date->diff($end_date);   // Difference between dates
+            if ($interval->y >= 1) {
+                if ($interval == 1) {
+                    $time_message = $interval->y . " year ago"; // 1 year ago
+                } else {
+                    $time_message = $interval->y . " year ago"; // 1+ year ago
+                }
+            } else if ($interval->m >= 1) {
+                if ($interval->d == 0) {
+                    $days = " ago";
+                } else if ($interval->d == 1) {
+                    $days = $interval->d . " day ago";
+                } else {
+                    $days = $interval->d . " days ago";
+                }
+                if ($interval->m == 1) {
+                    $time_message = $interval->m . " month" . $days;
+                } else {
+                    $time_message = $interval->m . " months" . $days;
+                }
+            } else if ($interval->d >= 1) {
+                if ($interval->d == 1) {
+                    $time_message = " Yesterday";
+                } else {
+                    $time_message = $interval->d . " days ago";
+                }
+            } else if ($interval->h >= 1) {
+                if ($interval->h == 1) {
+                    $time_message = $interval->h . " hour ago";
+                } else {
+                    $time_message = $interval->h . " hours ago";
+                }
+            } else if ($interval->i >= 1) {
+                if ($interval->i == 1) {
+                    $time_message = $interval->i . " minute ago";
+                } else {
+                    $time_message = $interval->i . " minutes ago";
+                }
+            } else {
+                if ($interval->s < 30) {
+                    $time_message = " Just now";
+                } else {
+                    $time_message = $interval->s . " seconds ago";
+                }
+            }
+
         }
     }
 }
+
