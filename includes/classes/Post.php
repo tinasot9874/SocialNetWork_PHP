655aa5479fcf7b87789d6fae359c5d9d946b590b
Hieu Tran <33086916+tinasot9874@users.noreply.github.com>
2020-03-12 02:02:37 +0700

    + fixed Get username + add function loadPostsFriends()

--- includes/classes/Post.php
+++ includes/classes/Post.php
@@ -21,9 +21,9 @@ class Post{
             $date_added = date("Y-m-d H:i:s");
 
             //Get username
-            $added_by = $this->user_obj->getFirstAndLastName();
+            $added_by = $this->user_obj->getUsername();
 
-            //If user is on own profile, user_to is 'none
+            //If user is on own profile, user_to is 'none'
             if ($user_to == $added_by){
                 $user_to = "none";
             }
@@ -36,8 +36,35 @@ class Post{
             //Update post count for user
             $num_posts = $this->user_obj->getNumPosts();
             $num_posts++;
-            $update_quey = mysqli_query($this->conn, "UPDATE users SET num_posts='$num_posts' WHERE username ='$added_by'");
+            $update_query = mysqli_query($this->conn, "UPDATE users SET num_posts='$num_posts' WHERE username ='$added_by'");
         }
     }
 
+    public function loadPostsFriends() {
+        $str = "";      // String to return
+        $data = mysqli_query($this->conn, "SELECT * FROM posts WHERE delete='no' ORDER BY id DESC");
+        while($row = mysqli_fetch_array($data)) {
+            $id = $row['id'];
+            $body = $row['body'];
+            $added_by = $row['added_by'];
+            $date_time = $row['date_added'];
+
+            // Prepare user_to string so it can be include even if not posted to a user
+            if ($row['user_to'] == "none"){
+                $user_to = "";
+            }else{
+                $user_to_obj = new User($conn, $row['user_to']);
+                $user_to_name = $user_to_obj->getFirstAndLastName();
+                $user_to = "<a href='" . $row['user_to'] . "'>" . $user_to_name . "</a>";
+            }
+            // Check if user who posted, has their account closed
+            $added_by_obj = new User($conn, $added_by);
+            if ($added_by_obj->isClosed()){
+                continue;
+            }
+            $user_details_query = mysqli_query($this->conn, "SELECT first_name, last_name, profile_pic FROM users WHERE username = '$added_by'");
+            $user_row = mysqli_fetch_array($user_details_query);
+
+        }
+    }
 }
