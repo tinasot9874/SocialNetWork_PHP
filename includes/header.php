dd00425a1a187d26e48a9e0edf968ce34706575a
Hieu Tran <33086916+tinasot9874@users.noreply.github.com>
2020-03-12 22:45:45 +0700

    + fixed

--- includes/header.php
+++ includes/header.php
@@ -15,7 +15,6 @@
 <head>
     <meta http-equiv="content-type" content="text/html; charset=utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="This is social network html5 template available in themeforest......" />
     <meta name="keywords" content="Social Network, Social Media, Make Friends, Newsfeed, Profile Page" />
     <meta name="robots" content="index, follow" />
     <title>News Feed | Check what your friends are doing</title>
